55ef96ef158bda9660f4e52928de2e0167186619
googolplex23 <48956705+googolplex23@users.noreply.github.com>
2023-01-19 15:45:47 -0600

    added SEE and delta pruning
    Also removed repetition detection and maybe broke checkmate value

--- engine.py
+++ engine.py
@@ -5,6 +5,7 @@ import sys
 from time import perf_counter_ns
 #from chess.engine import PlayResult
 from chess.polyglot import zobrist_hash
+from collections import Counter
 
 """
 class node():
@@ -20,8 +21,11 @@ class tamarac:
     def __init__(self):
         self.hash_table = {}
         self.quiescent_hash_table = {}
+        self.outcome_hash_table = {}
         self.nodes = 0
         self.revisit = 0
+        self.prev_guess = 0
+        self.tunable_values = [[-10000,-1000,-525,-350,-350,-100,0,100,350,350,525,1000,10000],1000]
         #self.node_table = {}
 
     
@@ -36,9 +40,9 @@ class tamarac:
         log.close()
         for entry in list(self.hash_table):
             curr_depth = self.hash_table[entry][1]
-            curr_depth = curr_depth + 1
+            curr_depth = curr_depth - 1
             self.hash_table[entry][1] = curr_depth
-            if curr_depth >= 5:
+            if curr_depth < 0:
                 del self.hash_table[entry]
         for entry in list(self.quiescent_hash_table):
             curr_depth = self.quiescent_hash_table[entry][1]
@@ -62,10 +66,12 @@ class tamarac:
             if board.is_capture(move):
                 captures.append(move)
         return captures
+    
+    #def check_threefold_repetition
                 
    
     def evaluate(self, board):  #interrim score function
-        weights = [-10000,-1000,-525,-350,-350,-100,0,100,350,350,525,1000,10000] #Scores of various pieces
+        weights = self.tunable_values[0] #Scores of various pieces
         board_int = self.convert_to_int(board)
         score = 0
         for x in board_int:
@@ -75,8 +81,29 @@ class tamarac:
        
         return score + len(list(board.legal_moves))
     
-    #def see_capture(self, board, move)
-    #    value = 
+    def see_capture(self, board, move):
+        piece = board.piece_type_at(move.to_square)
+        board.push(move)
+        value = self.tunable_values[0][piece+6] - self.see(board,move.to_square)
+        board.pop()
+        return value
+    
+    def see(self, board, square):
+        value = 0
+        if board.is_attacked_by(not board.turn,square):
+            pieces = board.attackers(not board.turn,square)
+            smallest = 100
+            best_square = None
+            for atk_square in pieces:
+                if board.piece_type_at(atk_square) < smallest:
+                    best_square = atk_square
+                    smallest = board.piece_type_at(atk_square)
+            board.push(board.find_move(best_square, square))
+            value = max(0,self.tunable_values[0][board.piece_type_at(square)+6] - self.see(board,square))
+            board.pop()
+        return value
+            
+            
     
     def order_moves(self, board):
         captures = []
@@ -91,7 +118,7 @@ class tamarac:
         
     def iterative_deepening(self,board,time):
         starttime = perf_counter_ns()
-        firstguess = {1:0}
+        firstguess = {1:self.prev_guess}
         depth = 0
         while True:
             depth = depth+1
@@ -102,6 +129,7 @@ class tamarac:
             firstguess[depth+2] = result[0]
             
             if perf_counter_ns() - starttime > time:
+                #self.prev_guess = 0-result[0]
                 return result[1]
   
     
@@ -116,7 +144,7 @@ class tamarac:
                 beta = g + 1
             else:
                 beta = g
-            result = self.failsoft_alphabeta(board, beta - 1, beta, depth)
+            result = self.failsoft_alphabeta(board, beta - 1, beta, 0, depth)
             g = result[0]
             if g < beta:
                 upperbound = g
@@ -238,30 +266,48 @@ class tamarac:
     def quiesce(self, board, alpha, beta): #quiescence search
     
         
+        try:
+            bestmove = list(board.legal_moves)[0]
+        except:
+            outcome = board.outcome()
+            if outcome.winner:
+                if outcome.winner == board.turn:
+                    return 100000000
+                else:
+                    return -100000000
+            return 0
+
+        #if board.can_claim_draw():
+        #    return -1250
     
         self.nodes = self.nodes + 1
+        """
         
         if zobrist_hash(board) in self.quiescent_hash_table:
             self.revisit = self.revisit +1
             return self.hash_table[zobrist_hash(board)][0]   
         
+        """
         #if zobrist_hash(board) in self.hash_table:        #check to make sure we haven't been here before
         #    return self.hash_table[zobrist_hash(board)]
         stand_pat = self.evaluate(board)
         if stand_pat >= beta:
             return beta
+        if stand_pat < alpha - 1000: # queen value
+            return alpha
         if alpha < stand_pat:
             alpha = stand_pat
         for move in list(board.legal_moves):
             if board.is_capture(move):
-                board.push(move)
-                score = -self.quiesce(board, -beta, -alpha)
-                if score >= beta:
-                    board.pop()
-                    return beta
-                if score > alpha:
-                    alpha = score
-                board.pop()
+                if self.see_capture(board, move) > 0:
+                    board.push(move)
+                    score = -self.quiesce(board, -beta, -alpha)
+                    if score >= beta:
+                        board.pop()
+                        return beta
+                    if score > alpha:
+                        alpha = score
+                        board.pop()
         
         return alpha
         
@@ -323,23 +369,28 @@ class tamarac:
     
     """
     
-    def failsoft_alphabeta(self, board, alpha, beta, depth):
+    def failsoft_alphabeta(self, board, alpha, beta, depth, maxdepth):
         self.nodes = self.nodes + 1
         
-        if depth == 0:
+        if depth == maxdepth:
             return [self.quiesce(board, alpha, beta)]
-        
         
         try:
             bestmove = list(board.legal_moves)[0]
         except:
-            if board.is_stalemate(): #this should prevent the bot from drawing the game if it's ahead but encourage it if it's losing
-                return [-950]
-            if board.is_checkmate():
-                return [1000000]
+            outcome = board.outcome()
+            if outcome.winner:
+                if outcome.winner == board.turn:
+                    return [100000000]
+                else:
+                    return [-100000000]
+            return [-1250]
+        """
+        if depth == :
+            if board.is_repetition():
+                return [-1250]
+        """
         
-        if board.is_repetition():
-            return [-950]
         
         ordered_moves = self.order_moves(board)
         if zobrist_hash(board) in self.hash_table:
@@ -349,7 +400,7 @@ class tamarac:
         bestscore = -10000000
         for move in ordered_moves:
             board.push(move)
-            result = self.failsoft_alphabeta(board,-beta,-alpha,depth-1)
+            result = self.failsoft_alphabeta(board,-beta,-alpha,depth+1,maxdepth)
             score = 0 - result[0]
             board.pop()
             if score >= beta:

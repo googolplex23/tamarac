8346eb512246b5f099c8af53a790c60456e0bee3
‘googolplex23’ <leogoogolplex@gmail.com>
2023-02-02 00:24:34 -0600

    added bad iterative deepening

--- tamarac-rust/src/main.rs
+++ tamarac-rust/src/main.rs
@@ -2,17 +2,25 @@
 
 use cozy_chess::{Move, Board, Square, Piece, Color};
 use std::io;
-use std::sync::atomic::{AtomicU64, Ordering};
-use std::time::{Duration, Instant};
+use std::sync::atomic::{AtomicU64, AtomicI32, Ordering};
+use std::time::{Instant, Duration};
 use vampirc_uci::parse;
-use vampirc_uci::{UciMessage, MessageList, UciTimeControl, Serializable};
+use vampirc_uci::{UciMessage, MessageList, UciTimeControl};
 
-const VALUES: ([i32; 6], i32) = ([100, 300, 300, 500, 900,10000],50); //king value must be greater than 9x queen + 2x rest of pieces
-const POS_INFINITY: i32 = i32::MAX - 256;
+const VALUES: ([i32; 6], i32, i32) = ([100, 300, 300, 500, 900,10000],50,40); //king value must be greater than 9x queen + 2x rest of pieces
+//const POS_INFINITY: i32 = i32::MAX - 256;
 const NEG_INFINITY: i32 = i32::MIN + 256;
 
 static NODE_CTR: AtomicU64 = AtomicU64::new(0);
+static SCORE: AtomicI32 = AtomicI32::new(0);
 
+/*
+struct PVtable(Vec<Entry>);
+
+struct Entry {
+	hash = 
+}
+*/
 fn list_moves(brd: &Board) -> Vec<Move> {
     let mut move_list = Vec::new();
 	brd.generate_moves(|moves| {
@@ -24,8 +32,9 @@ fn list_moves(brd: &Board) -> Vec<Move> {
 }
 
 fn evaluate(brd: &Board) -> i32 {
-	let mut total: i32 = 0;
+	let mut all_total: i32 = 0;
 	for clr in Color::ALL{
+		let mut total: i32 = 0;
 		for x in 0..5 {
 			let mut pieces: i32 = brd.colored_pieces(clr, *Piece::ALL.get(x).unwrap()).len().try_into().unwrap();
             /*
@@ -34,21 +43,19 @@ fn evaluate(brd: &Board) -> i32 {
             }
             */
             pieces = pieces * VALUES.0[x];
-			if clr == Color::Black {
-				pieces = 0 - pieces;
-			}
-			total = pieces + total;
+			total = total + pieces;
 		}
 		if clr == Color::Black {
 			total = 0 - total;
 		}
+		all_total = all_total + total
         
 	}
 	if brd.side_to_move() == Color::Black {
-		total = 0 - total;
+		all_total = 0 - all_total;
 	}
     //println!("{}", total);
-	total
+	all_total
 }
 
 fn alphabeta(brd: &Board, depth: u32, mut alpha: i32, beta: i32) -> i32 {
@@ -61,7 +68,7 @@ fn alphabeta(brd: &Board, depth: u32, mut alpha: i32, beta: i32) -> i32 {
 	for mov in move_list {
 		let mut new_brd = brd.clone();
 		new_brd.play(mov);
-		let score = alphabeta(&new_brd, depth - 1, 0 - beta, 0 - alpha);
+		let score = 0 - alphabeta(&new_brd, depth - 1, 0 - beta, 0 - alpha);
 		if score >= beta {
 			return score;
 		}
@@ -77,40 +84,42 @@ fn alphabeta(brd: &Board, depth: u32, mut alpha: i32, beta: i32) -> i32 {
 
 fn root_alphabeta(brd: &Board, depth: u32,) -> Move {
     NODE_CTR.store(NODE_CTR.load(Ordering::Relaxed) + 1, Ordering::Relaxed); //increment node count
-	let mut move_list = Vec::new();
-	brd.generate_moves(|moves| {
-        // Unpack dense move set into move list
-        move_list.extend(moves);
-        false
-    });
+	let move_list = list_moves(brd);
 	let mut bestscore = NEG_INFINITY;
 	let mut bestmove = None;
 	let mut alpha = NEG_INFINITY;
 	for mov in move_list {
 		let mut new_brd = brd.clone();
 		new_brd.play(mov);
-		let score = alphabeta(&new_brd, depth - 1, NEG_INFINITY, 0 - alpha);
+		let score = 0 - alphabeta(&new_brd, depth - 1, NEG_INFINITY, 0 - alpha);
 		if score > bestscore {
 			bestmove = Some(mov);
 			bestscore = score;
+			println!("info string score {score} bestscore {bestscore}");
 			if score > alpha {
 				alpha = score;
 			}
 		}
 	}
-    println!("info score cp {bestscore}");
+    SCORE.store(bestscore, Ordering::Relaxed);
 	bestmove.unwrap()
 }
 
-fn search(brd: &Board) -> Move {
+fn search(brd: &Board, time: &Duration) -> Move {
     NODE_CTR.store(0, Ordering::Relaxed);
     let start = Instant::now();
-    let mov = root_alphabeta(brd, 7);
-    let time = start.elapsed().as_millis() as u64;
+	let mut depth = 1;
+	let mut mov = None;
+	while start.elapsed() < *time {
+		mov = Some(root_alphabeta(brd, depth));
+		depth = depth + 1;
+	}
+    let end = start.elapsed().as_millis() as u64;
     let nodes = NODE_CTR.load(Ordering::Relaxed);
-    let nps = nodes*1000/time;
-    println!("info nodes {nodes} nps {nps} time {time}");
-    mov
+    let nps = nodes*1000/end;
+	let score = SCORE.load(Ordering::Relaxed);
+    println!("info depth {depth} nodes {nodes} nps {nps} time {end} score cp {score}");
+    mov.unwrap()
     
 }
 
@@ -128,7 +137,7 @@ fn check_castling_move(board: &Board, mut mv: Move) -> Move {
 }
 
 fn main() {
-	assert_eq!(Piece::ALL.get(0).unwrap(), &Piece::Pawn);
+	assert_eq!(0, evaluate(&Board::default()));
     let mut board = Board::default();
     loop {
         let mut cmd = String::new();
@@ -155,8 +164,26 @@ fn main() {
                     }
                 },
                 UciMessage::Quit => panic!(),
-                UciMessage::Go { time_control, search_control } => { //for some reason you need to put in a time control or it doesn't work. use go infinite for testing
-                    let bestmove = search(&board).to_string();
+                UciMessage::Go { time_control, search_control: _ } => { //for some reason you need to put in a time control or it doesn't work. use go infinite for testing
+					let mut time = Duration::ZERO;	
+					match time_control {
+						Some(UciTimeControl::MoveTime(movetime)) => {
+							time = movetime.to_std().unwrap();
+						},
+						Some(UciTimeControl::TimeLeft{white_time, black_time, white_increment: _, black_increment: _, moves_to_go: _}) => {
+							if white_time.is_some() && black_time.is_some() {
+								let mut remaining_ms = None;
+								if board.side_to_move() == Color::Black {
+									remaining_ms = Some(black_time.unwrap().to_std().unwrap());
+								} else {
+									remaining_ms = Some(white_time.unwrap().to_std().unwrap());
+								}
+								time = Duration::from_millis((remaining_ms.unwrap().as_millis() as i32 / VALUES.2).try_into().unwrap());
+							}
+						},
+						_ => time = Duration::new(5,0), 
+					}
+                    let bestmove = search(&board,&time).to_string();
                     println!("bestmove {bestmove}");
                 },
                 _ => (),
